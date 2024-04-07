dbcb80a57fa44ac5d6a2436a825ad367bfaf7b7e
RichardTry <bazrovstepan@gmail.com>
2024-04-07 17:40:31 +0100

    adding precommit and formatting files according to pep8

--- camera_view_generator/camera.py
+++ camera_view_generator/camera.py
@@ -1,29 +1,36 @@
-import torch
-import numpy as np
-from enum import Enum
-import trimesh
 import sys
-sys.path.append("..") 
-from tiny_nerf.utils.ray_utils import get_ray_directions, get_rays
-import numpy as np
+from enum import Enum
 
+import numpy as np
+import torch
+import trimesh
 from torch.utils.data import Dataset
 
+sys.path.append("..")
+from tiny_nerf.utils.ray_utils import get_ray_directions, get_rays  # noqa: E402
+
+
 class Directions(Enum):
-    FRONT = 0,      np.array([255, 0, 0, 255]),   'front'      # phi \in [-front/2, front/2)
-    SIDE_LEFT = 1,  np.array([0, 255, 0, 255]),   'side'       # phi \in [front/2, 180-front/2)
-    BACK = 2,       np.array([0, 0, 255, 255]),   'back'       # phi \in [180-front/2, 180+front/2)
-    SIDE_RIGHT = 3, np.array([255, 255, 0, 255]), 'side'     # phi \in [180+front/2, 360-front/2)
-    TOP = 4,        np.array([255, 0, 255, 255]), 'overhead' # theta \in [0, overhead]
-    BOTTOM = 5,     np.array([0, 255, 255, 255]), 'bottom'  # theta \in [180-overhead, 180]
+    FRONT = 0, np.array([255, 0, 0, 255]), "front"  # phi \in [-front/2, front/2)
+    SIDE_LEFT = 1, np.array([0, 255, 0, 255]), "side"  # phi \in [front/2, 180-front/2)
+    BACK = 2, np.array([0, 0, 255, 255]), "back"  # phi \in [180-front/2, 180+front/2)
+    SIDE_RIGHT = (
+        3,
+        np.array([255, 255, 0, 255]),
+        "side",
+    )  # phi \in [180+front/2, 360-front/2)
+    TOP = 4, np.array([255, 0, 255, 255]), "overhead"  # theta \in [0, overhead]
+    BOTTOM = 5, np.array([0, 255, 255, 255]), "bottom"  # theta \in [180-overhead, 180]
+
     def __int__(self):
         return self.value[0]
-    
+
     def get_color(self):
         return self.value[1]
-    
+
     def __str__(self):
         return self.value[2]
+
 
 def normalize(x):
     return x / torch.sqrt(torch.sum(x**2, keepdim=True, dim=-1))
@@ -33,22 +40,33 @@ def normalize(x):
 def get_directions_from_spherical(thetas, phis, overhead_angle, front_angle):
     dirs = np.empty(thetas.shape[0], dtype=Directions)
     # first determine by phis
-    #phis = phis % (2 * np.pi)
-    dirs[(phis < front_angle / 2) | (phis >= 2 * np.pi - front_angle / 2)] = Directions.FRONT
-    dirs[(phis >= front_angle / 2) & (phis < np.pi - front_angle / 2)] = Directions.SIDE_LEFT
-    dirs[(phis >= np.pi - front_angle / 2) & (phis < np.pi + front_angle / 2)] = Directions.BACK
-    dirs[(phis >= np.pi + front_angle / 2) & (phis < 2 * np.pi - front_angle / 2)] = Directions.SIDE_RIGHT
+    # phis = phis % (2 * np.pi)
+    dirs[
+        (phis < front_angle / 2) | (phis >= 2 * np.pi - front_angle / 2)
+    ] = Directions.FRONT
+    dirs[
+        (phis >= front_angle / 2) & (phis < np.pi - front_angle / 2)
+    ] = Directions.SIDE_LEFT
+    dirs[
+        (phis >= np.pi - front_angle / 2) & (phis < np.pi + front_angle / 2)
+    ] = Directions.BACK
+    dirs[
+        (phis >= np.pi + front_angle / 2) & (phis < 2 * np.pi - front_angle / 2)
+    ] = Directions.SIDE_RIGHT
     # override by thetas
     dirs[thetas <= overhead_angle] = Directions.TOP
     dirs[thetas >= (np.pi - overhead_angle)] = Directions.BOTTOM
     return dirs
 
 
-def generate_random_poses(size, theta_range = np.array([0, 180]),
-                          phi_range = np.array([0, 360]),
-                          radius_range = np.array([1, 1.5]),
-                          front_angle = 60,
-                          overhead_angle = 30):
+def generate_random_poses(
+    size,
+    theta_range=np.array([0, 180]),
+    phi_range=np.array([0, 360]),
+    radius_range=np.array([1, 1.5]),
+    front_angle=60,
+    overhead_angle=30,
+):
     # convert theta range and phi range to radians
     # as sin, cos function expect radians
     theta_range = theta_range * (np.pi / 180)
@@ -58,9 +76,9 @@ def generate_random_poses(size, theta_range = np.array([0, 180]),
 
     # generate spherical coordinates
     thetas = torch.rand(size) * (theta_range[1] - theta_range[0]) + theta_range[0]
-    phis =  torch.rand(size) * (phi_range[1] - phi_range[0]) + phi_range[0]
+    phis = torch.rand(size) * (phi_range[1] - phi_range[0]) + phi_range[0]
     radius = torch.rand(size) * (radius_range[1] - radius_range[0]) + radius_range[0]
-    #phis[phis < 0] += 2 * np.pi
+    # phis[phis < 0] += 2 * np.pi
     # transform spehrical coordinates to the point on the sphere in cartesian coordinates
     x = radius * torch.sin(thetas) * torch.cos(phis)
     y = radius * torch.sin(thetas) * torch.sin(phis)
@@ -85,10 +103,11 @@ def generate_random_poses(size, theta_range = np.array([0, 180]),
 
     return poses, directions
 
+
 # taken from https://github.com/ashawkey/stable-dreamfusion/blob/main/nerf/provider.py
 def visualize_poses(poses, dirs, size=0.1):
     # poses: [B, 4, 4], dirs: [B]
-    #print(poses.shape)
+    # print(poses.shape)
     axes = trimesh.creation.axis(axis_length=4)
     sphere = trimesh.creation.icosphere(radius=1)
     objects = [axes, sphere]
@@ -101,16 +120,20 @@ def visualize_poses(poses, dirs, size=0.1):
         c = pos - size * pose[:3, 0] - size * pose[:3, 1] - size * pose[:3, 2]
         d = pos + size * pose[:3, 0] - size * pose[:3, 1] - size * pose[:3, 2]
 
-        segs = np.array([[pos.numpy(), a.numpy()],
-                         [pos.numpy(), b.numpy()],
-                         [pos.numpy(), c.numpy()],
-                         [pos.numpy(), d.numpy()],
-                         [a.numpy(), b.numpy()],
-                         [b.numpy(), c.numpy()],
-                         [c.numpy(), d.numpy()],
-                         [d.numpy(), a.numpy()]])
-        
-        #print(segs)
+        segs = np.array(
+            [
+                [pos.numpy(), a.numpy()],
+                [pos.numpy(), b.numpy()],
+                [pos.numpy(), c.numpy()],
+                [pos.numpy(), d.numpy()],
+                [a.numpy(), b.numpy()],
+                [b.numpy(), c.numpy()],
+                [c.numpy(), d.numpy()],
+                [d.numpy(), a.numpy()],
+            ]
+        )
+
+        # print(segs)
         segs = trimesh.load_path(segs)
 
         # different color for different dirs
@@ -118,19 +141,21 @@ def visualize_poses(poses, dirs, size=0.1):
 
         objects.append(segs)
 
-    trimesh.Scene(objects).show(viewer='gl')
+    trimesh.Scene(objects).show(viewer="gl")
 
-def visualize_rays(origins, directions, size = 0.1):
+
+def visualize_rays(origins, directions, size=0.1):
     axes = trimesh.creation.axis(axis_length=4)
     objects = [axes]
 
     for origin, direction in zip(origins, directions):
         end = origin + direction
-        segs = np.array([origin.numpy(), end.numpy()], dtype='object')
+        segs = np.array([origin.numpy(), end.numpy()], dtype="object")
         segs = trimesh.load_path(segs)
         objects.append(segs)
 
-    trimesh.Scene(objects).show(viewer='gl')
+    trimesh.Scene(objects).show(viewer="gl")
+
 
 def generate_all_rays(size, H, W, focal):
     poses, dirs = generate_random_poses(size)
@@ -144,6 +169,7 @@ def generate_all_rays(size, H, W, focal):
 
     return all_rays, dirs
 
+
 class CameraDataset(Dataset):
     def __init__(self, label="default", H=64, W=64, samples=0):
         self.label = label
@@ -153,13 +179,14 @@ class CameraDataset(Dataset):
         self.default_fov = 20
         self.focal = self.H / (2 * np.tan(np.deg2rad(self.default_fov) / 2))
 
-        self.allrays, self.directions = generate_all_rays(self.samples, self.H, self.W, self.focal)
+        self.allrays, self.directions = generate_all_rays(
+            self.samples, self.H, self.W, self.focal
+        )
 
         self.allrays = self.allrays.reshape(samples, H * W, 6)
+
     def __len__(self):
         return self.samples
+
     def __getitem__(self, idx):
-        return {
-            'rays': self.allrays[idx],
-            'dirs': int(self.directions[idx])
-        }
+        return {"rays": self.allrays[idx], "dirs": int(self.directions[idx])}
--- tiny_nerf/nerf.py
+++ tiny_nerf/nerf.py
@@ -1,21 +1,20 @@
-import torch
-import numpy as np
-import torch.nn as nn
-from torch.utils.data import DataLoader
-#from utils.blender import BlenderDataset
-from rich.progress import Progress
 import os
-from rich.console import Console
-from PIL import Image
 
+import numpy as np
+import torch
+import torch.nn as nn
+from PIL import Image
+from rich.console import Console
+from rich.progress import Progress
 
 console = Console()
 
+
 def _take(chunk_size, rays):
-    batch_idxs = np.arange(0,rays.shape[0], chunk_size)
+    batch_idxs = np.arange(0, rays.shape[0], chunk_size)
     for batch_start in batch_idxs:
-        batch_end = batch_start+chunk_size
-        batch_end = rays.shape[0] if batch_end>rays.shape[0] else batch_end
+        batch_end = batch_start + chunk_size
+        batch_end = rays.shape[0] if batch_end > rays.shape[0] else batch_end
         yield rays[batch_start:batch_end, :]
 
 
@@ -31,43 +30,62 @@ def test(model, hn, hf, dataset, chunk_size=10, img_index=0, nb_bins=192, H=400,
         nb_bins (int, optional): number of bins for density estimation. Defaults to 192.
         H (int, optional): image height. Defaults to 400.
         W (int, optional): image width. Defaults to 400.
-        
+
     Returns:
         None: None
     """
-   
+    device = "cpu"
     os.makedirs("novel_views", exist_ok=True)
-    data = []   # list of regenerated pixel values
+    data = []  # list of regenerated pixel values
     example = dataset[img_index]
     for chunk in _take(chunk_size, example["rays"]):
         # iterate over chunks
         ray_origins = chunk[:, :3].to(device)
         ray_directions = chunk[:, 3:6].to(device)
-        
-        regenerated_px_values = render_rays(model, ray_origins, ray_directions, hn=hn, hf=hf, nb_bins=nb_bins) 
+
+        regenerated_px_values = render_rays(
+            model, ray_origins, ray_directions, hn=hn, hf=hf, nb_bins=nb_bins
+        )
         data.append(regenerated_px_values)
     img = torch.cat(data).data.cpu().numpy().reshape(H, W, 3)
 
-    pil_img = Image.fromarray((img*255).astype(np.uint8))
+    pil_img = Image.fromarray((img * 255).astype(np.uint8))
     pil_img.save(f"novel_views/img_{img_index}.png")
 
 
 class NerfModel(nn.Module):
-    def __init__(self, embedding_dim_pos=10, embedding_dim_direction=4, hidden_dim=128):   
+    def __init__(self, embedding_dim_pos=10, embedding_dim_direction=4, hidden_dim=128):
         super(NerfModel, self).__init__()
-        
-        self.block1 = nn.Sequential(nn.Linear(embedding_dim_pos * 6 + 3, hidden_dim), nn.ReLU(),
-                                    nn.Linear(hidden_dim, hidden_dim), nn.ReLU(),
-                                    nn.Linear(hidden_dim, hidden_dim), nn.ReLU(),
-                                    nn.Linear(hidden_dim, hidden_dim), nn.ReLU(), )
+
+        self.block1 = nn.Sequential(
+            nn.Linear(embedding_dim_pos * 6 + 3, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU(),
+        )
         # density estimation
-        self.block2 = nn.Sequential(nn.Linear(embedding_dim_pos * 6 + hidden_dim + 3, hidden_dim), nn.ReLU(),
-                                    nn.Linear(hidden_dim, hidden_dim), nn.ReLU(),
-                                    nn.Linear(hidden_dim, hidden_dim), nn.ReLU(),
-                                    nn.Linear(hidden_dim, hidden_dim + 1), )
+        self.block2 = nn.Sequential(
+            nn.Linear(embedding_dim_pos * 6 + hidden_dim + 3, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim + 1),
+        )
         # color estimation
-        self.block3 = nn.Sequential(nn.Linear(embedding_dim_direction * 6 + hidden_dim + 3, hidden_dim // 2), nn.ReLU(), )
-        self.block4 = nn.Sequential(nn.Linear(hidden_dim // 2, 3), nn.Sigmoid(), )
+        self.block3 = nn.Sequential(
+            nn.Linear(embedding_dim_direction * 6 + hidden_dim + 3, hidden_dim // 2),
+            nn.ReLU(),
+        )
+        self.block4 = nn.Sequential(
+            nn.Linear(hidden_dim // 2, 3),
+            nn.Sigmoid(),
+        )
 
         self.embedding_dim_pos = embedding_dim_pos
         self.embedding_dim_direction = embedding_dim_direction
@@ -77,61 +95,101 @@ class NerfModel(nn.Module):
     def positional_encoding(x, L):
         out = [x]
         for j in range(L):
-            out.append(torch.sin(2 ** j * x))
-            out.append(torch.cos(2 ** j * x))
+            out.append(torch.sin(2**j * x))
+            out.append(torch.cos(2**j * x))
         return torch.cat(out, dim=1)
 
     def forward(self, o, d):
-        emb_x = self.positional_encoding(o, self.embedding_dim_pos) # emb_x: [batch_size, embedding_dim_pos * 6]
-        emb_d = self.positional_encoding(d, self.embedding_dim_direction) # emb_d: [batch_size, embedding_dim_direction * 6]
-        h = self.block1(emb_x) # h: [batch_size, hidden_dim]
-        tmp = self.block2(torch.cat((h, emb_x), dim=1)) # tmp: [batch_size, hidden_dim + 1]
-        h, sigma = tmp[:, :-1], self.relu(tmp[:, -1]) # h: [batch_size, hidden_dim], sigma: [batch_size]
-        h = self.block3(torch.cat((h, emb_d), dim=1)) # h: [batch_size, hidden_dim // 2]
-        c = self.block4(h) # c: [batch_size, 3]
+        emb_x = self.positional_encoding(
+            o, self.embedding_dim_pos
+        )  # emb_x: [batch_size, embedding_dim_pos * 6]
+        emb_d = self.positional_encoding(
+            d, self.embedding_dim_direction
+        )  # emb_d: [batch_size, embedding_dim_direction * 6]
+        h = self.block1(emb_x)  # h: [batch_size, hidden_dim]
+        tmp = self.block2(
+            torch.cat((h, emb_x), dim=1)
+        )  # tmp: [batch_size, hidden_dim + 1]
+        h, sigma = tmp[:, :-1], self.relu(
+            tmp[:, -1]
+        )  # h: [batch_size, hidden_dim], sigma: [batch_size]
+        h = self.block3(torch.cat((h, emb_d), dim=1))  # h: [batch_size, hidden_dim // 2]
+        c = self.block4(h)  # c: [batch_size, 3]
         return c, sigma
 
 
 def compute_accumulated_transmittance(alphas):
     accumulated_transmittance = torch.cumprod(alphas, 1)
-    return torch.cat((torch.ones((accumulated_transmittance.shape[0], 1), device=alphas.device),
-                      accumulated_transmittance[:, :-1]), dim=-1)
+    return torch.cat(
+        (
+            torch.ones((accumulated_transmittance.shape[0], 1), device=alphas.device),
+            accumulated_transmittance[:, :-1],
+        ),
+        dim=-1,
+    )
 
 
 def render_rays(nerf_model, ray_origins, ray_directions, hn=0, hf=0.5, nb_bins=192):
     device = ray_origins.device
-    
-    t = torch.linspace(hn, hf, nb_bins, device=device).expand(ray_origins.shape[0], nb_bins)
+
+    t = torch.linspace(hn, hf, nb_bins, device=device).expand(
+        ray_origins.shape[0], nb_bins
+    )
     # Perturb sampling along each ray.
-    mid = (t[:, :-1] + t[:, 1:]) / 2.
+    mid = (t[:, :-1] + t[:, 1:]) / 2.0
     lower = torch.cat((t[:, :1], mid), -1)
     upper = torch.cat((mid, t[:, -1:]), -1)
     u = torch.rand(t.shape, device=device)
     t = lower + (upper - lower) * u  # [batch_size, nb_bins]
-    delta = torch.cat((t[:, 1:] - t[:, :-1], torch.tensor([1e10], device=device).expand(ray_origins.shape[0], 1)), -1)
+    delta = torch.cat(
+        (
+            t[:, 1:] - t[:, :-1],
+            torch.tensor([1e10], device=device).expand(ray_origins.shape[0], 1),
+        ),
+        -1,
+    )
 
     # Compute the 3D points along each ray
-    x = ray_origins.unsqueeze(1) + t.unsqueeze(2) * ray_directions.unsqueeze(1)   # [batch_size, nb_bins, 3]
+    x = ray_origins.unsqueeze(1) + t.unsqueeze(2) * ray_directions.unsqueeze(
+        1
+    )  # [batch_size, nb_bins, 3]
     # Expand the ray_directions tensor to match the shape of x
-    ray_directions = ray_directions.expand(nb_bins, ray_directions.shape[0], 3).transpose(0, 1) 
+    ray_directions = ray_directions.expand(nb_bins, ray_directions.shape[0], 3).transpose(
+        0, 1
+    )
 
     colors, sigma = nerf_model(x.reshape(-1, 3), ray_directions.reshape(-1, 3))
     colors = colors.reshape(x.shape)
     sigma = sigma.reshape(x.shape[:-1])
 
     alpha = 1 - torch.exp(-sigma * delta)  # [batch_size, nb_bins]
-    weights = compute_accumulated_transmittance(1 - alpha).unsqueeze(2) * alpha.unsqueeze(2)
+    weights = compute_accumulated_transmittance(1 - alpha).unsqueeze(2) * alpha.unsqueeze(
+        2
+    )
     # Compute the pixel values as a weighted sum of colors along each ray
     c = (weights * colors).sum(dim=1)
-    weight_sum = weights.sum(-1).sum(-1)  # Regularization for white background 
+    weight_sum = weights.sum(-1).sum(-1)  # Regularization for white background
     return c + 1 - weight_sum.unsqueeze(-1)
 
 
-def train(batch_size, nerf_model, optimizer, scheduler, data_loader, guidance, device='cpu', hn=0, hf=1, nb_epochs=int(1e5),
-          nb_bins=192, H=400, W=400):
+def train(
+    batch_size,
+    nerf_model,
+    optimizer,
+    scheduler,
+    data_loader,
+    guidance,
+    device="cpu",
+    hn=0,
+    hf=1,
+    nb_epochs=int(1e5),
+    nb_bins=192,
+    H=400,
+    W=400,
+):
     model_save_path = "model_saved.pt"
     training_loss = []
-   
+
     if os.path.exists(model_save_path):
         nerf_model.load_state_dict(torch.load(model_save_path))
     else:
@@ -139,28 +197,44 @@ def train(batch_size, nerf_model, optimizer, scheduler, data_loader, guidance, d
             epochs_task = progress.add_task("[red]Epoch...", total=nb_epochs)
 
             for epoch_idx in range(nb_epochs):
-                progress.update(epochs_task, description=f"[red]Epoch {epoch_idx}/{nb_epochs}")
+                progress.update(
+                    epochs_task, description=f"[red]Epoch {epoch_idx}/{nb_epochs}"
+                )
                 batch_task = progress.add_task("[green]Batcd", total=len(data_loader))
-                
+
                 for idx, batch in enumerate(data_loader):
-                    
                     ray_origins = batch["rays"][:, :3].to(device)
                     ray_directions = batch["rays"][:, 3:6].to(device)
                     concrete_directions = batch["dirs"].to(device)
-                    
-                    regenerated_px_values = render_rays(nerf_model, ray_origins, ray_directions, hn=hn, hf=hf, nb_bins=nb_bins) 
-                
-                    #loss = ((ground_truth_px_values - regenerated_px_values) ** 2).sum()
-                    loss = guidance.calculate_loss(regenerated_px_values, batch_size, H, W, concrete_directions)
+
+                    regenerated_px_values = render_rays(
+                        nerf_model,
+                        ray_origins,
+                        ray_directions,
+                        hn=hn,
+                        hf=hf,
+                        nb_bins=nb_bins,
+                    )
+
+                    # loss = ((ground_truth_px_values - regenerated_px_values) ** 2).sum()
+                    loss = guidance.calculate_loss(
+                        regenerated_px_values, batch_size, H, W, concrete_directions
+                    )
                     optimizer.zero_grad()
                     loss.backward()
                     optimizer.step()
                     training_loss.append(loss.item())
-                    progress.update(batch_task, advance=1, description=f"[green]Batch {idx}/{len(data_loader)} loss: {np.mean(training_loss)}")
+                    progress.update(
+                        batch_task,
+                        advance=1,
+                        description=f"[green]Batch {idx}/{len(data_loader)}"
+                        f"loss: {np.mean(training_loss)}",
+                    )
                 scheduler.step()
                 progress.update(epochs_task, advance=1)
             torch.save(nerf_model.state_dict(), model_save_path)
-       
-    #for img_index in range(200):
-    #    test(nerf_model, hn, hf, testing_dataset, chunk_size=1024, img_index=img_index, nb_bins=nb_bins, H=H, W=W)
+
+    # for img_index in range(200):
+    #    test(nerf_model, hn, hf, testing_dataset, chunk_size=1024,
+    #    img_index=img_index, nb_bins=nb_bins, H=H, W=W)
     return training_loss
